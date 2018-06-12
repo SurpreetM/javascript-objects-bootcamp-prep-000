1a291d2e496dd7a0be7b46d2fb73f159d0694b80
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-06-12 15:02:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist (obj, artistName, songTitle) {
  return obj
 }
 
-function removeFromPlaylist (obj, artistName) {
-  delete obj.artistName
-  return obj
+function removeFromPlaylist (playlist, artistName) {
+  delete playlist.artistName
+  return playlist
 }
