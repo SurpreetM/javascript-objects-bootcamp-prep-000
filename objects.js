484ac5ecac05e8213e68fbb12f7f1e457208f8ff
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-06-12 14:54:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {Slowdive: "Alison"}
 
 function updatePlaylist (obj, artistName, songTitle) {
- return Object.assign({}, playlist {[artistName]:"songTitle"})
+ return Object.assign({}, playlist, {[artistName]:"songTitle"})
 }
